4c28737cb3ae5835fd6ccf6a7b316dc6339c0b1a
gdusbabek <gdusbabek@gmail.com>
2020-05-15 13:29:13 -0500

    get rid of native encoding. use a direwolf string instead

--- floater/aprs.py
+++ floater/aprs.py
@@ -1,26 +1,5 @@
 import os
-import struct
 from datetime import datetime
-from bitstring import BitArray, Bits
-import audiogen
-from floater import afsk
-
-#  the AX.25 frame
-# 1 byte      : flag = b'\x7e'
-# 7 bytes     : dest address, right padded, has a format
-# 7 bytes     : source address, right padded, as a format
-# 0-56 bytes  : digipeater addresses
-# 1 byte      : control field = b'\x03'
-# 1 byte      : protocol id = b'\xf0'
-# 1-256 bytes : information field, format varies, but usually contains a header
-# 2 bytes     : FCS checksum
-# 1 byte      : flag, same as first.
-
-
-FLAG = b'\x7e'
-CTRL = b'\x03'
-PCOL = b'\xf0'
-BASE_91_DIGITS = [chr(i + 33) for i in range(91)]
 
 class BalloonInfo(object):
     def __init__(self):
@@ -36,85 +15,6 @@ class BalloonInfo(object):
 
 def zulu_now():
     return datetime.utcnow()
-
-def encode_call(addr):
-    # split into (addr, ssid)
-    addr = addr.upper()
-    if addr.find('-') > 0:
-        addr, ssid = addr.split('-')
-    else:
-        ssid = '0'
-
-    # adjust addr, ssid in case they are out of range.
-    addr = addr[:6] if len(addr) > 6 else addr
-    ssid = ssid[:1] if len(ssid) > 1 else ssid
-
-    # convert to bytes and shift left one bit.
-    call = f"{addr:6s}{ssid}".encode('ascii')
-    call_bytes = BitArray(call)
-    call_bytes.rol(1)
-
-    return call_bytes.tobytes()
-
-def decode_call(call_bytes):
-    # read in and shift right one bit.
-    cb = BitArray(call_bytes)
-    cb.ror(1)
-    call = cb.tobytes().decode('ascii')
-
-    # separate call and ssid. ssid is at the end. there may be whitespace in between them.
-    ssid = call[-1]
-    call = call[:-1].strip()
-    return f"{call}-{ssid}"
-
-def encode_digis(digis):
-    # encoded_digi_array = [encode_call(d) for d in digis]
-    # return b''.join(encoded_digi_array)
-
-    encoded_digi_array = bytearray(b''.join([encode_call(d) for d in digis]))
-
-    # saw this getting done somewhere...
-    # I don't think it should really matter. Here's why:
-    # during decode everything gets shifted one bit to the right, so this falls off.
-    # the argument for it was to know when the digis end, but the control field does
-    # a good job of that, so whatevs.
-    # I'm doing this for sake of compatibility.
-    encoded_digi_array[-1] |= 0x01
-
-    return encoded_digi_array
-
-def decode_digis(digis_bytes):
-    if len(digis_bytes) % 7 != 0:
-        raise RuntimeError(f"Invalid array length; cannot decode digis ({len(digis_bytes)}")
-
-    digis_bytes = bytearray(digis_bytes)  # make it mutable so the next line works.
-
-    # unscrew the last byte.
-    digis_bytes[-1] &= 0xfe
-
-    digis = [digis_bytes[n*7:(n+1)*7] for n in range(len(digis_bytes) // 7)]  # noqa: E226
-    digis = [decode_call(d) for d in digis]
-    return digis
-
-def encode_base91(num):
-    if num < 0:
-        raise ValueError(f"Input should be >= zero ({num})")
-    elif num == 0:  # easy case.
-        return BASE_91_DIGITS[0]
-    digits = []
-    while num:
-        digits.append(BASE_91_DIGITS[int(num % 91)])
-        num //= 91
-    digits.reverse()
-    return ''.join(digits)
-
-## BYTES DEMARCATOR: everything below here does not return bytes. Maybe fix that?
-
-def decode_base91(s):
-    num = 0
-    for i, ch in enumerate(s):
-        num += BASE_91_DIGITS.index(ch) * (91 ** (len(s) - 1 - i))
-    return num
 
 # don't use this format in status reports.
 def getHMS(dt):
@@ -136,17 +36,11 @@ def _encode_arbitrary_location(lonlat, w):
     return f"{dm}.{h}{d}"
 
 def encode_latitude(lat):
-    """
-    input will probably be ddmm.hhhhhN
-    convert it into ddmm.hhM
-    """
+    """ input will probably be ddmm.hhhhhN, convert it into ddmm.hhM """
     return _encode_arbitrary_location(lat, 4)
 
 def encode_longitude(lon):
-    """
-    input will probably be dddmm.hhhhhW.
-    convert it to dddmm.hhW
-    """
+    """ input will probably be dddmm.hhhhhW, convert it to dddmm.hhW """
     return _encode_arbitrary_location(lon, 5)
 
 def encode_position(lat, lon, table='/', symbol='O'):
@@ -184,52 +78,5 @@ def make_info_string(balloon, dt):
     # we still have room for 36-9=27 bytes in the comment.
     return data
 
-def make_fcs(byte_data):
-    fcs = 0xffff
-    for byte in byte_data:
-        for i in range(7, -1, -1):
-            bit = (byte >> i) & 0x01 == 1
-            check = fcs & 0x1 == 1
-            fcs >>= 1
-            if check != bit:
-                fcs ^= 0x8408
-
-    # make the digest
-    return struct.pack('<H', ~fcs % 2**16)
-
-def packet(balloon_info, destination, via_digis, zulu_dt):
-    # these are bytes
-    dst = encode_call(destination)
-    src = encode_call(balloon_info.call)
-    digis = encode_digis(via_digis)
-
-    # info is a string, so be sure to convert it to bytes.
-    info = make_info_string(balloon_info, zulu_dt).encode('ascii')
-
-    data = dst + src + digis + CTRL + PCOL + info
-    fcs = make_fcs(data)
-
-    # i hate myself.
-    def stuff(byte_data):
-        count = 0
-        for bit in afsk._as_bits(byte_data):
-            if bit:
-                count += 1
-            else:
-                count = 0
-            yield bit
-            if count == 5:
-                yield False
-                count = 0
-
-    return FLAG + BitArray(stuff(data + fcs)).tobytes() + FLAG
-
-def packet_to_wav(packet, file_path):
-    if os.path.exists(file_path):
-        os.remove(file_path)
-    if os.path.exists(file_path):
-        raise RuntimeError(f"Could not remove {file_path}")
-
-    audio = afsk.encode(packet)
-    with open(file_path, 'wb') as f:
-        audiogen.sampler.write_wav(f, audio)
+def make_direwolf_string(bln, dst, via_digis, dt):
+    return f"{bln.call}>{dst},{','.join(via_digis)}:{make_info_string(bln, dt)}"
--- tests/test_aprs.py
+++ tests/test_aprs.py
@@ -22,49 +22,6 @@ def simple_zulu():
     dt = datetime.strptime("05/13/20 14:21:59", "%m/%d/%y %H:%M:%S")
     return dt
 
-# def test_generator_detection():
-#     def gen_func():
-#         for x in range(5):
-#             yield x
-
-#     ## test some baseline assumptions about pass by reference.
-#     def mutate_list(l):
-#         l[0] = 100
-#     def really_mutate_list(l):
-#         l.pop(0)
-#     my_list = [x for x in range(5)]
-#     assert my_list == [0,1,2,3,4]
-#     mutate_list(my_list)
-#     assert my_list == [100,1,2,3,4]
-#     really_mutate_list(my_list)
-#     assert my_list == [1,2,3,4]
-
-#     assert len(gen_func()) == 1
-
-def test_encode_call():
-    assert aprs.encode_call('AAAAAA-11') == b'\x82\x82\x82\x82\x82\x82b'
-    assert aprs.encode_call('BBBB-66') == b'\x84\x84\x84\x84@@l'
-    assert aprs.encode_call('cccc') == b'\x86\x86\x86\x86@@`'
-    assert aprs.encode_call('APRS') == b'\x82\xa0\xa4\xa6@@`'
-
-def test_decode_call():
-    assert aprs.decode_call(b'\x82\x82\x82\x82\x82\x82b') == 'AAAAAA-1'
-    assert aprs.decode_call(b'\x84\x84\x84\x84@@l') == 'BBBB-6'
-    assert aprs.decode_call(b'\x86\x86\x86\x86@@`') == 'CCCC-0'
-    assert aprs.decode_call(b'\x82\xa0\xa4\xa6@@`') == 'APRS-0'
-
-def test_encode_digis():
-    assert aprs.encode_digis(['WIDE1-1', 'RELAY']) == b'\xae\x92\x88\x8ab@b\xa4\x8a\x98\x82\xb2@a'
-
-def test_decode_digis():
-    assert aprs.decode_digis(b'\xae\x92\x88\x8ab@b\xa4\x8a\x98\x82\xb2@`') == ['WIDE1-1', 'RELAY-0']
-
-def test_encode_base91():
-    assert len(aprs.BASE_91_DIGITS) == 91
-    assert aprs.encode_base91(12345678) == '1Cmi'
-
-def test_decode_base91():
-    assert aprs.decode_base91('1Cmi') == 12345678
 
 def test_get_times():
     dt = datetime.strptime("05/13/20 14:21:59", "%m/%d/%y %H:%M:%S")
@@ -99,23 +56,7 @@ def test_encode_course_and_speed():
 def test_make_info_string(simple_balloon, simple_zulu):
     assert aprs.make_info_string(simple_balloon, simple_zulu) == '@142159h4903.50N/07201.75WO065/156/A=075345'
 
-def test_fcs():
-    data = b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    fcs_digest = aprs.make_fcs(data)
-    assert fcs_digest == b'[\x07'
-
-def test_make_aprs_packet(simple_balloon, simple_zulu):
-    packet = aprs.packet(simple_balloon, "APRS", ["WIDE1-1", "WIDE2-1"], simple_zulu)
-    expected = b"~\x82\xa0\xa4\xa6@@`\x9c`\x86\x82\x98\x98`\xae\x92\x88\x8ab@b\xae\x92\x88\x8ad@c\x03\xe8 \x18\x9a\x19\x18\x9a\x9c\xb4\x1a\x1c\x98\x19\x97\x1a\x98'\x17\x98\x1b\x99\x18\x18\x97\x1b\x9a\xab\xa7\x98\x1b\x1a\x97\x98\x9a\x9b\x17\xa0\x9e\x98\x1b\x9a\x99\x9a\x1a\x90\xe9\x80~"
-    assert packet == expected
-
-def test_make_wav(simple_balloon, simple_zulu):
-    # clean up any debris
-    wav_path = '/tmp/test_file.wav'
-    if os.path.exists(wav_path):
-        os.remove(wav_path)
-    assert not os.path.exists(wav_path)
-
-    packet = aprs.packet(simple_balloon, 'APRS', ['WIDE1-1', 'WIDE2-1'], simple_zulu)
-    aprs.packet_to_wav(packet, wav_path)
-    assert os.path.exists(wav_path)
+def test_make_direwolf_string(simple_balloon, simple_zulu):
+    # echo -n "N0CALL>APN25,WIDE1-1,WIDE2-1:@142159h4903.50N/07201.75WO065/156/A=075345" | ./gen_packets -a 25 -o /tmp/test_file_3.wav -
+    direwolf_str = aprs.make_direwolf_string(simple_balloon, 'APRS', ["WIDE1-1", "WIDE2-1"], simple_zulu)
+    assert direwolf_str == 'N0CALL>APN25,WIDE1-1,WIDE2-1:@142159h4903.50N/07201.75WO065/156/A=075345 in=45 out=-23'
